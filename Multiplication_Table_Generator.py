7b4dfe2477014d6589e1ccf07dbcc20650152fb0
Thomas Martin <87400753+tm4rtin17@users.noreply.github.com>
2025-01-02 18:51:49 -0800

    Update Multiplication_Table_Generator.py
    1. Updated from a recursive function to an infinite loop to avoid stack overflow situations.
    2. Removed unnecessary 'sys.exit()' function

--- Multiplication_Table_Generator.py
+++ Multiplication_Table_Generator.py
@@ -1,12 +1,11 @@
 import pyinputplus as pyip
-import sys
 
 #Define Function
 def create_table():
 
     #Define Variables
     base_number = pyip.inputNum("What is the base number you would like to generate a multiplication table for?")
-    max_row_number = pyip.inputNum("What is the maximum amount of rows you would like to generate?", min=1)
+    max_row_number = pyip.inputNum("How many rows would you like to generate?", min=1)
     row_counter = 1
 
     #Loop and Print Results
@@ -14,14 +13,17 @@ def create_table():
         result = base_number * row_counter
         print(f"{base_number} X {row_counter} = {result}")
         row_counter += 1
-    
-    #User Input to Determine Next Step
-    next_step = pyip.inputYesNo("Would you like to create another multiplication table? (y/n)")
-    
-    # 'yes' response re-runs the function, 'no' answer ends the script
+
+#Run Initial Function  
+create_table()
+
+#Loop allows the user infinite table creations
+while True:
+    next_step = pyip.inputYesNo("Would you like to create another multiplication table? (y/n)") #(y/n) User Input to Determine Next Step
     if next_step == 'yes':
         create_table()
     else:
-        sys.exit()
+        print("Thank you for using the Multiplication Table Generator! Goodbye!")
+        break
 
-create_table()
+
